0145dca10cd303839c25ae885d07ea1da0ffd552
Vishal Surya <vishalsurya2701@gmail.com>
2024-07-07 18:43:48 +0530

    logo and images

--- src/app/store/assessments.constants.ts
+++ src/app/store/assessments.constants.ts
@@ -15,8 +15,8 @@ export const assessmentConfigs: { [key: string]: AssessmentConfig } = {
   },
   MIDAS: {
     title: 'MIDAS',
-    logo: 'https://images.thrively.com/v7/https://api.filerob…a5778b50000/midas-circle.png?func=bound&w=80&h=80',
-    image: 'https://images.thrively.com/v7/https://api.filerob…-9af7-57cd-9b61-5cbef3a50000/assessment-intro.png',
+    logo: "https://images.thrively.com/v7/https://api.filerobot.com/thrively/v5/get/e5772b3a-fadd-5b0f-aa85-9dfe79850000/midas_print_logo.png",
+    image: "https://images.thrively.com/v7/https://api.filerobot.com/thrively/v5/get/08b8051f-9af7-57cd-9b61-5cbef3a50000/assessment-intro.png",
     info: 'The Multiple Intelligences Developmental Assessment Scales (aka MIDAS) aptitude test takes about 25 minutes to complete. There are 8 areas of activities, skills, and interests covered.',
     description1: `You'll be asked questions in each area and be given multiple answers to choose from. Pick the answer that best matches your experience`,
     description2: `Remember, your results will only be as accurate as your answers. It's OK to say that you don't know.`,
@@ -27,8 +27,8 @@ export const assessmentConfigs: { [key: string]: AssessmentConfig } = {
   },
   THOMAS: {
     title: 'Thomas',
-    logo: `https://images.thrively.com/v7/https://api.filerobot.com/thrively/v5/get/8bf444e1-1002-5787-a09a-05c5f3150000/thomas_print_logo.png?func=bound&w=80&h=80`,
-    image: 'https://images.thrively.com/v7/https://api.filerob…-9af7-57cd-9b61-5cbef3a50000/assessment-intro.png',
+    logo: "https://images.thrively.com/v7/https://api.filerobot.com/thrively/v5/get/8bf444e1-1002-5787-a09a-05c5f3150000/thomas_print_logo.png?func=bound&w=80&h=80",
+    image: 'https://images.thrively.com/v7/https://api.filerobot.com/thrively/v5/get/08b8051f-9af7-57cd-9b61-5cbef3a50000/assessment-intro.png',
     info: 'The Habits of Mind assessment takes about 25 minutes to complete. There are 16 habits explored across social, emotional, physical, and mental aspects of your character.',
     description1: `You'll be asked questions in each area and be given multiple answers to choose from. Pick the answer that best matches your experience`,
     description2: `Remember, your results will only be as accurate as your answers. It's OK to say that you don't know.`,
@@ -39,8 +39,8 @@ export const assessmentConfigs: { [key: string]: AssessmentConfig } = {
   },
   RIASEC: {
     title: 'RIASEC',
-    logo: '	https://images.thrively.com/v7/https://api.filerob…t/cf1d6c2d-e888-5104-a0d7-5fcc5d450000/riasec.jpg',
-    image: 'https://images.thrively.com/v7/https://api.filerob…2aa850000/riasec_intro.png?func=bound&w=440&h=160',
+    logo: "https://images.thrively.com/v7/https://api.filerobot.com/thrively/v5/get/cf1d6c2d-e888-5104-a0d7-5fcc5d450000/riasec.jpg",
+    image: 'https://images.thrively.com/v7/https://api.filerobot.com/thrively/v5/get/633aa880-e222-5d82-8164-b822aa850000/riasec_intro.png?func=bound&w=440&h=160',
     info: 'The Thrively Interest Profiler can help you zero in on your interests and better understand how those might best connect to the world of work.',
     description1: 'Read each question carefully, and consider how you might feel doing each type of work. These responses will help determine your top personality types.',
     description2: 'There are no wrong or right answers! You are simply learning more about how your interests match up with future career pathways',
